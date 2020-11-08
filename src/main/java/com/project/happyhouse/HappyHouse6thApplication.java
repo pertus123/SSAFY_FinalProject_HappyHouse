ebedfff6c9b9bc9fb69bcaf3fee6fc363ede07a8
LEEAYOUNG <lay0711@naver.com>
2020-11-08 14:14:07 +0900

    [Add] test

---
+++ src/main/java/com/project/happyhouse/HappyHouse6thApplication.java
@@ -0,0 +1,13 @@
+package com.project.happyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHouse6thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyHouse6thApplication.class, args);
+	}
+
+}
