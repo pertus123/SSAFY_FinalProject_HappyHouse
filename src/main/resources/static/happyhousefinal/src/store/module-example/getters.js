b0abcc013cc13ff788407c24cc38a20275625c22
최동식 <opert1564@naver.com>
2020-12-16 11:25:37 +0900

    remove file

--- src/main/resources/static/happyhousefinal/src/store/module-example/getters.js
+++
@@ -1,9 +0,0 @@
-export function getAccessToken(state) {
-  return state.accessToken;
-}
-export function getUserId(state) {
-  return state.userId;
-}
-export function getUserName(state) {
-  return state.userName;
-}
--- src/main/resources/static/happyhousefinal/src/store/module-example/state.js
+++
@@ -1,7 +0,0 @@
-export default function () {
-  return {
-    accessToken: null,
-    userId: "",
-    userName: ""
-  }
-}
